869a10e491d2e0054b656849884cad2a720caafb
Eyuel Mulugeta <eyuelmulugeta60@gmail.com>
2023-11-26 20:22:59 +0300

    add files to github

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+a = input("Enter first number ")
+b = input("Enter second number ")
+x = int(a)
+y = int (b)
+
+print("Sum:")
+print(x + y)
+print("Difference:")
+print(x - y)
+print("Product:")
+print(x * y)
+print("Quetiont")
+print(x / y)
