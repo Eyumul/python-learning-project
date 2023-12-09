acd7ff696966337cffb46025832a8801f78273d2
Eyuel Mulugeta <eyuelmulugeta60@gmail.com>
2023-12-09 23:19:42 +0300

    tic_tac_toe 2.0

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,5 +1,40 @@
 import random
-while True:
+def play(value,pc):
+        num = int(value)
+        if choice == value:
+                if pos[num - 1] == " ":
+                    pos[num -1] = sign
+                    pcvalue.append(num-1)
+                    while pc in pcvalue and len(pcvalue) != 9:
+                        pc = random.randint(0,8)
+                    pcvalue.append(pc)
+                    pos[pc] = pcsign
+                else:
+                    print("You cheated")
+                    return 0
+def grid(a,b,c,d,e,f,g,h,i):
+        print("     |     |     ")
+        print("  " + a + "  |  " + b + "  |  " + c + "  ")
+        print("_____|_____|_____")
+        print("     |     |     ")
+        print("  " + d + "  |  " + e + "  |  " + f + "  ")
+        print("_____|_____|_____")
+        print("     |     |     ")
+        print("  " + g + "  |  " + h + "  |  " + i + "  ")
+        print("     |     |     ")
+        print(" ")
+def check(a0,a1,a2,a3,a4,a5,a6,a7,a8,checksign):
+        a = a0 == a1 == a2 == checksign
+        b = a3 == a4 == a5 == checksign
+        c = a6 == a7 == a8 == checksign
+        d = a0 == a3 == a6 == checksign
+        e = a1 == a4 == a7 == checksign
+        f = a2 == a5 == a8 == checksign
+        g = a0 == a4 == a8 == checksign
+        h = a2 == a4 == a6 == checksign
+        abc = a or b or c or d or e or f or g or h
+        return abc
+while True:           
     pos = ["1","2","3","4","5","6","7","8","9"]
     prompt = ["$$$!!!YOU WIN!!!$$$","YOU LOOSE","It's a draw"]
     j = 1
@@ -7,7 +42,6 @@ while True:
     l = 0
     pcvalue = []
     pc = random.randint(0,8)
-
     while j <= 2:
         print("Choose 'X' or 'O' to play: ")
         sign = input().upper()
@@ -19,216 +53,36 @@ while True:
             break
         else:
             print("Please enter valid INPUT: ")
-
-    print("     |     |     ")
-    print("  " + pos[0] + "  |  " + pos[1] + "  |  " + pos[2] + "  ")
-    print("_____|_____|_____")
-    print("     |     |     ")
-    print("  " + pos[3] + "  |  " + pos[4] + "  |  " + pos[5] + "  ")
-    print("_____|_____|_____")
-    print("     |     |     ")
-    print("  " + pos[6] + "  |  " + pos[7] + "  |  " + pos[8] + "  ")
-    print("     |     |     ")
-    print(" ")
-
+    grid(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8])
     while l <= 8:
         pos[l] = " "
         l += 1
-
     while j <= 5:
         print("Enter the number")
-        value = input()
-        if value == "1":
-            if pos[0] == " ":
-                pos[0] = sign
-                pcvalue.append(0)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "2":
-            if pos[1] == " ":
-                pos[1] = sign
-                pcvalue.append(1)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "3":
-            if pos[2] == " ":
-                pos[2] = sign
-                pcvalue.append(2)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "4":
-            if pos[3] == " ":
-                pos[3] = sign
-                pcvalue.append(3)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "5":
-            if pos[4] == " ":
-                pos[4] = sign
-                pcvalue.append(4)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "6":
-            if pos[5] == " ":
-                pos[5] = sign
-                pcvalue.append(5)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "7":
-            if pos[6] == " ":
-                pos[6] = sign
-                pcvalue.append(6)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "8":
-            if pos[7] == " ":
-                pos[7] = sign
-                pcvalue.append(7)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        elif value == "9":
-            if pos[8] == " ":
-                pos[8] = sign
-                pcvalue.append(8)
-                if " " not in pos:
-                    break
-                while pc in pcvalue:
-                    pc = random.randint(0,8)
-                pcvalue.append(pc)
-                pos[pc] = pcsign
-            else:
-                print("You cheated")
-                break
-        else:
-            print("INVALID INPUT")
-            break
-        print("     |     |     ")
-        print("  " + pos[0] + "  |  " + pos[1] + "  |  " + pos[2] + "  ")
-        print("_____|_____|_____")
-        print("     |     |     ")
-        print("  " + pos[3] + "  |  " + pos[4] + "  |  " + pos[5] + "  ")
-        print("_____|_____|_____")
-        print("     |     |     ")
-        print("  " + pos[6] + "  |  " + pos[7] + "  |  " + pos[8] + "  ")
-        print("     |     |     ")
-        print(" ")
-
-        a = pos[0] == pos[1] == pos[2] == sign
-        b = pos[3] == pos[4] == pos[5] == sign
-        c = pos[6] == pos[7] == pos[8] == sign
-        d = pos[0] == pos[3] == pos[6] == sign
-        e = pos[1] == pos[4] == pos[7] == sign
-        f = pos[2] == pos[5] == pos[8] == sign
-        g = pos[0] == pos[4] == pos[8] == sign
-        h = pos[2] == pos[4] == pos[6] == sign
-        abc = a or b or c or d or e or f or g or h
-        a2 = pos[0] == pos[1] == pos[2] == pcsign
-        b2 = pos[3] == pos[4] == pos[5] == pcsign
-        c2 = pos[6] == pos[7] == pos[8] == pcsign
-        d2 = pos[0] == pos[3] == pos[6] == pcsign
-        e2 = pos[1] == pos[4] == pos[7] == pcsign
-        f2 = pos[2] == pos[5] == pos[8] == pcsign
-        g2 = pos[0] == pos[4] == pos[8] == pcsign
-        h2 = pos[2] == pos[4] == pos[6] == pcsign
-        abc2 = a2 or b2 or c2 or d2 or e2 or f2 or g2 or h2
-        if  abc:
+        choice = input()
+        while choice != "1" and choice != "2" and choice != "3" and choice != "4" and choice != "5" and choice != "6" and choice != "7" and choice != "8" and choice != "9":
+             print("INVALID INPUT\n Please Tryagain: ")
+             grid(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8])
+             choice = input()
+        if play(choice,pc) == 0:
+             break
+        grid(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8])
+        if  check(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],sign):
             print(prompt[0])
             break
-        elif  abc2:
+        elif  check(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],pcsign):
             print(prompt[1])
+            break
+        if " " not in pos:
+            grid(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8])
+            if not check(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],sign) and not check(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],pcsign):
+                print(prompt[2])
+            elif check(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],sign):
+                print(prompt[0])
+            elif check(pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],pcsign):
+                print(prompt[1])
             break
         j += 1
-    if " " not in pos:
-        a = pos[0] == pos[1] == pos[2] == sign
-        b = pos[3] == pos[4] == pos[5] == sign
-        c = pos[6] == pos[7] == pos[8] == sign
-        d = pos[0] == pos[3] == pos[6] == sign
-        e = pos[1] == pos[4] == pos[7] == sign
-        f = pos[2] == pos[5] == pos[8] == sign
-        g = pos[0] == pos[4] == pos[8] == sign
-        h = pos[2] == pos[4] == pos[6] == sign
-        abc = a or b or c or d or e or f or g or h
-        a2 = pos[0] == pos[1] == pos[2] == pcsign
-        b2 = pos[3] == pos[4] == pos[5] == pcsign
-        c2 = pos[6] == pos[7] == pos[8] == pcsign
-        d2 = pos[0] == pos[3] == pos[6] == pcsign
-        e2 = pos[1] == pos[4] == pos[7] == pcsign
-        f2 = pos[2] == pos[5] == pos[8] == pcsign
-        g2 = pos[0] == pos[4] == pos[8] == pcsign
-        h2 = pos[2] == pos[4] == pos[6] == pcsign
-        abc2 = a2 or b2 or c2 or d2 or e2 or f2 or g2 or h2
-        print("     |     |     ")
-        print("  " + pos[0] + "  |  " + pos[1] + "  |  " + pos[2] + "  ")
-        print("_____|_____|_____")
-        print("     |     |     ")
-        print("  " + pos[3] + "  |  " + pos[4] + "  |  " + pos[5] + "  ")
-        print("_____|_____|_____")
-        print("     |     |     ")
-        print("  " + pos[6] + "  |  " + pos[7] + "  |  " + pos[8] + "  ")
-        print("     |     |     ")
-        print(" ")
-        if not abc and not abc2:
-            print(prompt[2])
-        elif abc:
-            print(prompt[0])
-        elif abc2:
-            print(prompt[1])
     outlet=input("do you want to stop playing yes/no?").lower()
     if outlet=="yes":
         break
